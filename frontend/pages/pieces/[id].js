f8bb23f02fec378c9c6444441f71aba13d86ded0
miquelvir <41950283+miquelvir@users.noreply.github.com>
2023-04-29 17:41:42 +0200

    fix build

--- frontend/pages/pieces/[id].js
+++ frontend/pages/pieces/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 
-export default ({}) => {
+export default function PiecePage ({}){
     const {id} = useRouter()?.query
     return <div>
         Here will go the page for piece: {id}
