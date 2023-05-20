655bc0f2548f9acb30c92277b81cd5337f8cf151
miquelvir <41950283+miquelvir@users.noreply.github.com>
2023-05-20 18:24:45 +0200

    choose .env depending on ENV

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from .login import with_login
-
-from dotenv import load_dotenv
-load_dotenv()
 import os
+from dotenv import load_dotenv
+load_dotenv(".env.development" if os.environ.get('ENV', None) == 'dev' else ".env.production")
+
 
 app = Flask(__name__)
 CORS(app)
