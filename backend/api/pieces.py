ccb95ab240f4a29b8ec19f9b920314b88ecb5de3
miquelvir <41950283+miquelvir@users.noreply.github.com>
2023-05-28 19:33:40 +0200

    fix id bug

--- backend/api/pieces.py
+++ backend/api/pieces.py
@@ -19,14 +19,14 @@ def get_pieces():
             row_dict[columns[i]] = row[i]
         latent_map = json.loads(row_dict["latent_map"])
         result.append({
-                "title": row_dict["work_title"],
-                "period": row_dict["composer_period"],
-                "author": row_dict["composer"],
-                "difficulty": {
-                    "x1": latent_map[0],
-                    "x2": latent_map[1],
-                "id": row_dict["musicsheetid"]
-            }
+            "title": row_dict["work_title"],
+            "period": row_dict["composer_period"],
+            "author": row_dict["composer"],
+            "difficulty": {
+                "x1": latent_map[0],
+                "x2": latent_map[1]
+            },
+            "id": row_dict["musicsheetid"]
         })
 
     return result
