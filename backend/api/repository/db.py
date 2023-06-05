0c16c228f356c6e0e582e446f118c679dcaaaf45
miquelvir <41950283+miquelvir@users.noreply.github.com>
2023-06-05 12:10:04 +0200

    remove logs

--- backend/api/repository/db.py
+++ backend/api/repository/db.py
@@ -6,7 +6,6 @@ import os
 load_dotenv(find_dotenv())
 
 def get_connection():
-    print(3)
     return psycopg2.connect(
         database=os.getenv("DATABASE_NAME"),
         host=os.getenv("DATABASE_HOST"),
@@ -19,20 +18,12 @@ def get_connection():
 
 @contextlib.contextmanager
 def database():
-    print(4)
     conn = get_connection()
-    print(5)
     cursor = conn.cursor()
-    print(6)
     try:
-        print(7)
         yield cursor
-        print(8)
-    except e:
-        print(8)
-        print(e)
+    except:
         raise
     finally:
         cursor.close()
         conn.close()
-    print(9)
