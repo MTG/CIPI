df624a9a8a2b1a8d1712d61e15f624cbb7d3a8a0
miquelvir <41950283+miquelvir@users.noreply.github.com>
2023-06-05 17:28:02 +0200

    try fix build

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from backend.login import with_login
+from ._auth.login import with_login
 import os
 from dotenv import load_dotenv
-from backend.repository.pieces import get_pieces 
-from backend.clients.pdf_difficulty_service import get_difficulty
+from ._repository.pieces import get_pieces 
+from ._clients.pdf_difficulty_service import get_difficulty
 
 load_dotenv(".env.development" if os.environ.get('ENV', None) == 'dev' else ".env.production")
 load_dotenv(".env")
