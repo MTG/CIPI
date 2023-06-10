a11dcf679777520657699ed943e1614711587d63
anira-b <anira.besora01@estudiant.upf.edu>
2023-06-10 20:53:39 +0200

    changed_filter_function

--- backend/api/repository/filtering_functions.py
+++ backend/api/repository/filtering_functions.py
@@ -3,7 +3,34 @@ def apply_filters(page, cursor, size, key, period, min_difficulty, max_difficult
   
     #eliminate from array the None values and the corresponding column from database
       
-    query = "SELECT COUNT(*) FROM musicsheet WHERE"
+    query_count = "SELECT COUNT(*) FROM musicsheet WHERE"
+    query_select = "SELECT * FROM musicsheet WHERE"
+  
+    query_filters, filtered_values= query_filters(key, period, min_difficulty, max_difficulty)
+
+    query_count= query_count+query_filters
+    cursor.execute(query_count, filtered_values)
+
+    total_pages = cursor.fetchone()[0]
+   
+    # Ensure the page number is within the valid range
+    if page < 1:
+        page = 1
+    elif page > total_pages:
+        page = total_pages
+
+    offset = (page - 1) * size
+
+    query_select = query_select + query_filters + "  LIMIT %(limit)s OFFSET %(offset)s"
+    filtered_values["limit"] = size
+    filtered_values["offset"] = offset
+
+    cursor.execute(query_select, filtered_values )
+   
+    return cursor, total_pages
+
+def query_filters(key, period, min_difficulty, max_difficulty):
+    query=""
     filtered_values={}
 
     if key is not None:
@@ -16,38 +43,7 @@ def apply_filters(page, cursor, size, key, period, min_difficulty, max_difficult
         query= query + " normalized_difficulty BETWEEN %(min_difficulty)s AND %(max_difficulty)s AND"
         filtered_values["min_difficulty"] = min_difficulty
         filtered_values["max_difficulty"] = max_difficulty
-    #difficulty amb una mitja dels 3 valors--> normalitzar i afegir a una columna
     words = query.split()
     words_without_last = words[:-1]  # Exclude the last and
-
     query = ' '.join(words_without_last)
-
-    cursor.execute(query, filtered_values)
-
-    total_pages = cursor.fetchone()[0]
-   
-    # Ensure the page number is within the valid range
-    if page < 1:
-        page = 1
-    elif page > total_pages:
-        page = total_pages
-
-    offset = (page - 1) * size
-
-    words = query.split()
-    for i in range(len(words)):
-        if words[i] == "COUNT(*)":
-            words[i] = "*"
-            break
-
-    query = ' '.join(words)
-
-    query = query + "  LIMIT %(limit)s OFFSET %(offset)s"
-    filtered_values["limit"] = size
-    filtered_values["offset"] = offset
-
-    cursor.execute(query, filtered_values )
-   
-    
-    return cursor, total_pages
-
+    return query, filtered_values
