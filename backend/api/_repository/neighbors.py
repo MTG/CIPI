f10b19a2cec4eba08e3b52cb13bbbfc4b0a4a005
miquelvir <41950283+miquelvir@users.noreply.github.com>
2023-06-11 15:30:36 +0200

    fix and show neighbours in pdf upload

--- backend/api/_repository/neighbors.py
+++ backend/api/_repository/neighbors.py
@@ -1,33 +1,52 @@
 from .db import database
-from .pieces import db_dict
+from .pieces import to_piece_dto, get_row_dict
 import numpy as np
 
-def get_piece_neighbors(id, size):
-    with database() as cursor:
+
+def _get_latent_map(piece):
+    return np.array([float(piece["latent_map_x1"]), float(piece["latent_map_x2"])])
+
+
+def _get_piece_neighbors(pieces, target_latent_map, size):
+    print(1, target_latent_map)
+    target_latent_map = target_latent_map[:2]
+    print(2, target_latent_map)
+    # Convert the latent_map arrays to NumPy arrays
+    latent_maps = list(map(_get_latent_map, pieces))
+    
+    print(3, latent_maps[0])
+
+    # Calculate the Euclidean distances between the target and all other latent_map arrays
+    distances = [np.linalg.norm(target_latent_map - latent_map) for latent_map in latent_maps]
+    
+    # Sort the distances and get the indices of the closest entries
+    closest_indices = np.argsort(distances)[:size]
+    
+    return [to_piece_dto(pieces[i]) for i in closest_indices]
+        
+
+def _fetch_all_pieces():
+     with database() as cursor:
         cursor.execute("SELECT * FROM musicsheet")
         
         columns = [desc[0] for desc in cursor.description]
-        rows = cursor.fetchall()
-        
-        # Get the latent_map array for the input ID
-        target_latent_map = None
-        for row in rows:
-            if row[0] == id:
-                target_latent_map = np.array(eval(row[17]))
-                break
-        
-        # Convert the latent_map arrays to NumPy arrays
-        latent_maps = [np.array(eval(row[17])) for row in rows]
-        
-        # Calculate the Euclidean distances between the target and all other latent_map arrays
-        distances = [np.linalg.norm(target_latent_map - latent_map) for latent_map in latent_maps]
-        
-        # Sort the distances and get the indices of the closest entries
-        closest_indices = np.argsort(distances)[:size]
-        
-        closest_entries = []
-        db_dict([rows[i] for i in closest_indices], columns, closest_entries)
+        return [get_row_dict(row, columns) for row in cursor.fetchall()]
 
-        return closest_entries
-        
+
+def get_piece_neighbours_by_latent_map(target_latent_map, size):
+    pieces = _fetch_all_pieces()
+    return _get_piece_neighbors(pieces, target_latent_map, size)
+
+
+def get_piece_neighbors_by_id(id, size):
+    pieces = _fetch_all_pieces()
+    # Get the latent_map array for the input ID
+    target_latent_map = None
+    for piece in pieces:
+        if piece['musicsheetid'] == id:
+            target_latent_map = _get_latent_map(piece)
+            break
+    
+    return _get_piece_neighbors(pieces, target_latent_map, size)
+
         
--- backend/api/_repository/pieces.py
+++ backend/api/_repository/pieces.py
@@ -1,25 +1,27 @@
 from .db import database
 import json
 
-def db_dict(rows, columns, result):
-    for row in rows:
-            row_dict = {}
-            for i in range(len(columns)):
-                row_dict[columns[i]] = row[i]
-            result.append({
-                "url": row_dict["url"],
-                "title": row_dict["work_title"],
-                "period": row_dict["composer_period"],
-                "author": row_dict["composer"],
-                "year": row_dict["first_publication"],
-                "difficulty": {
-                    "x1": row_dict["latent_map_x1"],
-                    "x2": row_dict["latent_map_x2"]
-                },
-                "id": row_dict["musicsheetid"],
-                "key": row_dict["_key"]
-            })
-    return result
+def get_row_dict(row, columns):
+    row_dict = {}
+    for i in range(len(columns)):
+        row_dict[columns[i]] = row[i]
+    return row_dict
+
+def to_piece_dto(row_dict):
+    return {
+        "url": row_dict["url"],
+        "title": row_dict["work_title"],
+        "period": row_dict["composer_period"],
+        "author": row_dict["composer"],
+        "year": row_dict["first_publication"],
+        "difficulty": {
+            "x1": row_dict["latent_map_x1"],
+            "x2": row_dict["latent_map_x2"]
+        },
+        "id": row_dict["musicsheetid"],
+        "key": row_dict["_key"]
+    }
+
 
 def get_pieces():
     with database() as cursor:
@@ -27,16 +29,13 @@ def get_pieces():
         columns = [desc[0] for desc in cursor.description]
         rows = cursor.fetchall()
 
-    result = []
-    pieces = db_dict(rows, columns, result)
-
-    return pieces
+    pieces = (get_row_dict(row, columns) for row in rows)
+    return list(map(to_piece_dto, pieces))
 
 def get_pieces_id(id):
-    result = []
     with database() as cursor:
         cursor.execute('SELECT * FROM musicsheet WHERE musicsheetid=%(id)s', {"id": id})
         columns = [desc[0] for desc in cursor.description]
         rows = cursor.fetchall()
-        piece=db_dict(rows, columns, result)
-    return piece[0]
+        return to_piece_dto(get_row_dict(rows[0], columns))
+
