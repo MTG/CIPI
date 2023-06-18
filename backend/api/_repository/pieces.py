ed0313a1f1e8b6cc4a4e26ea02b06450ca49154a
AndreaTortato <atortato@tradeheader.com>
2023-06-18 22:18:21 +0200

    finally done BE filtering

--- backend/api/_repository/pieces.py
+++ backend/api/_repository/pieces.py
@@ -65,3 +65,23 @@ def get_pieces_id(id):
         return rows
     #to_piece_dto(get_row_dict(rows[0], columns))
 
+def get_row_dict(row, columns):
+    row_dict = {}
+    for i in range(len(columns)):
+        row_dict[columns[i]] = row[i]
+    return row_dict
+
+def to_piece_dto(row_dict):
+    return {
+        "url": row_dict["url"],
+        "title": row_dict["work_title"],
+        "period": row_dict["composer_period"],
+        "author": row_dict["composer"],
+        "year": row_dict["first_publication"],
+        "difficulty": {
+            "x1": row_dict["latent_map_x1"],
+            "x2": row_dict["latent_map_x2"]
+        },
+        "id": row_dict["musicsheetid"],
+        "key": row_dict["_key"]
+    }
